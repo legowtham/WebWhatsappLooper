6c8aa1d58bf9c249e7df46a1e90e2961a8701e33
Gowtham Mathivanan <007gowthamm@gmail.com>
2017-07-27 20:23:33 +0530

    Add files via upload

---
+++ whatsapplooper.js
@@ -0,0 +1,20 @@
+//Use the console with web.whatsapp.com opened
+//select the person's chat screen
+//Run the code. 
+var intervalID = setInterval(function () {
+function findReactComponent(dom) {
+    for (var key in dom)
+        if (key.startsWith("__reactInternalInstance$")) {
+            var compInternals = dom[key]._currentElement;
+            var compWrapper = compInternals._owner;
+            var comp = compWrapper._instance;
+            return comp;
+        }
+    return null;
+};
+var inputComponent = findReactComponent(document.getElementsByClassName('block-compose')[0]); // get the react component for the chat input
+var chat = inputComponent.props.chat;
+
+
+chat.sendMessage('I love you every second baby!!')// LOL! She will be impressed af!
+}, 1000); //Just remove the time interval to remove the loop process.
